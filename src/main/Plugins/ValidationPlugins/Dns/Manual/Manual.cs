db339f01d3ccc289d99b42649be065bc7d7dd88b
Eric Candell <eric@candell.org>
2019-05-28 07:39:16 -0700

    Add code to manual DNS validation to play with self-hosted

--- src/main/Plugins/ValidationPlugins/Dns/Manual/Manual.cs
+++ src/main/Plugins/ValidationPlugins/Dns/Manual/Manual.cs
@@ -1,13 +1,17 @@
 ﻿using PKISharp.WACS.Clients.DNS;
 using PKISharp.WACS.Services;
 using System.Linq;
+using System.Threading.Tasks;
+using DNS.Server;
+using DNS.Client;
 
 namespace PKISharp.WACS.Plugins.ValidationPlugins.Dns
 {
     class Manual : DnsValidation<ManualOptions, Manual>
     {
         private IInputService _input;
-
+        private MasterFile masterFile;
+        private DnsServer server;
         public Manual(
             LookupClientProvider dnsClient,  
             ILogService log, 
@@ -21,17 +25,34 @@ namespace PKISharp.WACS.Plugins.ValidationPlugins.Dns
             // only and therefor we will allow it. Future versions might be more advanced,
             // e.g. shoot an email to an admin and complete the order later.
             _input = input;
+            masterFile = new MasterFile();
+            server = new DnsServer(masterFile, "8.8.8.8");
+            server.Responded += (sender, e) => { _input.Show("{e.Request} => {e.Response}"); };
+            server.Listening += (sender, e) => _input.Show("Listening for DNS requests");
+            server.Errored += (sender, e) => {
+                _input.Show("Errored: {e.Exception}");
+                ResponseException responseError = e.Exception as ResponseException;
+                if (responseError != null) _input.Show(responseError.Response.ToString());
+                masterFile.AddTextResourceRecord("_acme-challenge.candell.org.", "", "hiTokenTest");
+            };
+            server.Listen();
         }
-        
-        public override void CreateRecord(string recordName, string token)
+
+
+    public override void CreateRecord(string recordName, string token)
         {
-            _input.Show("Domain", _identifier, true);
-            _input.Show("Record", recordName);
-            _input.Show("Type", "TXT");
-            _input.Show("Content", $"\"{token}\"");
-            _input.Show("Note 1", "Some DNS control panels add quotes automatically. Only one set is required.");
-            _input.Show("Note 2", "Make sure your name servers are synchronised, this may take several minutes!");
+
+            masterFile.AddTextResourceRecord(recordName, "", token);
+            _input.Show("Create an NS record for {recordname} pointing to this server");
             _input.Wait("Please press enter after you've created and verified the record");
+
+            //_input.Show("Domain", _identifier, true);
+            //_input.Show("Record", recordName);
+            //_input.Show("Type", "TXT");
+            //_input.Show("Content", $"\"{token}\"");
+            //_input.Show("Note 1", "Some DNS control panels add quotes automatically. Only one set is required.");
+            //_input.Show("Note 2", "Make sure your name servers are synchronised, this may take several minutes!");
+            //_input.Wait("Please press enter after you've created and verified the record");
 
             // Pre-pre-validate, allowing the manual user to correct mistakes
             while (true)
@@ -57,6 +78,7 @@ namespace PKISharp.WACS.Plugins.ValidationPlugins.Dns
 
         public override void DeleteRecord(string recordName, string token)
         {
+            server.Dispose();
             _input.Show("Domain", _identifier, true);
             _input.Show("Record", recordName);
             _input.Show("Type", "TXT");
