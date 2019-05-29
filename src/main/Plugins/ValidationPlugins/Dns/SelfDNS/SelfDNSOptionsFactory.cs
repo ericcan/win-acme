b7fc8822df3e2fa2999bfe193ab0e866c5e45007
Eric Candell <eric@candell.org>
2019-05-29 09:39:36 -0700

    Perform test of port 53

--- src/main/Plugins/ValidationPlugins/Dns/SelfDNS/SelfDNSOptionsFactory.cs
+++ src/main/Plugins/ValidationPlugins/Dns/SelfDNS/SelfDNSOptionsFactory.cs
@@ -3,6 +3,7 @@ using PKISharp.WACS.Plugins.Base.Factories;
 using PKISharp.WACS.Services;
 using PKISharp.WACS.Clients.DNS;
 using DNS.Server;
+using System;
 
 namespace PKISharp.WACS.Plugins.ValidationPlugins.Dns
 {
@@ -34,18 +35,24 @@ namespace PKISharp.WACS.Plugins.ValidationPlugins.Dns
             //and then doing a lookup for a test record or just seeing whether the event fires.
             testDNSRecords.AddTextResourceRecord("_acme-challenge.testdomain.org", "", "custom TXTrecord");
             server.Listen();
-            //if the port appears to be open, then check the each of the targets to make sure that the specific
-            //identifiers are set up by adding records for each and making sure that they produce results.
+            var testResponse = _dnsClient.DefaultClient.GetTextRecordValues("_acme-challenge.testdomain.org").ToString();
+            if (testResponse == "custom TXTrecord")
+            {
+                _log.Information("Port 53 appears to be opened correctly");
+            } else { 
+                //if the port appears to be open, then check the each of the targets to make sure that the specific
+                //identifiers are set up by adding records for each and making sure that they produce results.
 
-            //if these tests don't yield results, tell user to set up
-            _log.Information("To set up self-hosted DNS validation, insure the following:");
-            _log.Information("--You have opened port 53 in your firewall for incoming requests");
-            _log.Information("--You have created a NS DNS record for _acme-challenge...., pointing to your server's web address");
-            _log.Information("To test your setup, port 53 is now open and you may perform a TXT query for");
-            _log.Information("_acme-challenge.testdomain.org, specifying your server as the DNS Server.");
-            _log.Information(" You should get a TXT value of 'custom TXTrecord'");
+                //if these tests don't yield results, tell user to set up
+                _log.Information("To set up self-hosted DNS validation, insure the following:");
+                _log.Information("--You have opened port 53 in your firewall for incoming requests");
+                _log.Information("--You have created a NS DNS record for _acme-challenge...., pointing to your server's web address");
+                _log.Information("To test your setup, port 53 is now open and you may perform a TXT query for");
+                _log.Information("_acme-challenge.testdomain.org, specifying your server as the DNS Server.");
+                _log.Information(" You should get a TXT value of 'custom TXTrecord'");
 
-            _input.Wait("Press ENTER once you have opened the port.\n");
+                _input.Wait("Press ENTER once you have opened the port.\n");
+            }
             server.Dispose();
             return new SelfDNSOptions();
         }
