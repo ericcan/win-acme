de71b06ee0c53e9e26ddcb1fe3365324c8fb16ef
Eric Candell <eric@candell.org>
2019-05-28 19:40:36 -0700

    remove input service and use log

--- src/main/Plugins/ValidationPlugins/Dns/SelfDNS/SelfDNS.cs
+++ src/main/Plugins/ValidationPlugins/Dns/SelfDNS/SelfDNS.cs
@@ -10,59 +10,49 @@ namespace PKISharp.WACS.Plugins.ValidationPlugins.Dns
 {
     class SelfDNS : DnsValidation<SelfDNSOptions, SelfDNS>
     {
-        private IInputService _input;
         private MasterFile masterFile;
         private DnsServer server;
         public SelfDNS(
             LookupClientProvider dnsClient,
             ILogService log,
-            IInputService input,
             SelfDNSOptions options, string
             identifier) :
             base(dnsClient, log, options, identifier)
         {
-            // Usually it's a big no-no to rely on user input in validation plugin
-            // because this should be able to run unattended. This plugin is for testing
-            // only and therefor we will allow it. Future versions might be more advanced,
-            // e.g. shoot an email to an admin and complete the order later.
-            _input = input;
             masterFile = new MasterFile();
             server = new DnsServer(masterFile, "8.8.8.8");
-            server.Responded += (sender, e) => { _input.Show("REQ:", e.Request.ToString()); _input.Show("RES:", e.Response.ToString()); };
-            server.Listening += (sender, e) => _input.Show("Listening for DNS requests");
+            server.Responded += (sender, e) => 
+            {
+                _log.Information("DNS Server received lookup request" );
+                _log.Debug("DNS Response: " + e.Request.ToString());
+                _log.Debug("DNS Response: " + e.Response.ToString());
+            };
+            server.Listening += (sender, e) => _log.Information("DNS Server is listening on Port 53. Make sure your firewall has opened this port");
             server.Errored += (sender, e) =>
             {
-                _input.Show("Errored: ${e.Exception}");
+                _log.Debug("Errored: {Error}", e.Exception);
                 ResponseException responseError = e.Exception as ResponseException;
-                if (responseError != null) _input.Show(responseError.Response.ToString());
+                if (responseError != null) _log.Debug(responseError.Response.ToString());
             };
         }
-
 
         public override void CreateRecord(string recordName, string token)
         {
             masterFile.AddTextResourceRecord(recordName, "", token);
-            masterFile.AddNameServerResourceRecord(recordName, "8.8.8.8");
+            masterFile.AddNameServerResourceRecord(recordName, "aws.candell.org"); //need to replace with user parameter
         }
         public override void PrepareChallenge()
         {
             CreateRecord(_challenge.DnsRecordName, _challenge.DnsRecordValue);
+            _log.Information("Validation token added to DNS Server TXT for {answerUri}", _challenge.DnsRecordName);
             server.Listen();
 
-            _log.Information("Answer should now be available at {answerUri}", _challenge.DnsRecordName);
-
-            // Verify that the record was created succesfully and wait for possible
-            // propagation/caching/TTL issues to resolve themselves naturally
-            if (!PreValidate())
-            {
-                _log.Information("It looks like validation is going to fail, but we will try now anyway...");
-
-            }
-
+            PreValidate();
         }
         public override void DeleteRecord(string recordName, string token)
         {
             server.Dispose();
+            _log.Information("DNS Server terminated from Port 53");
         }
         protected new bool PreValidate()
         {
