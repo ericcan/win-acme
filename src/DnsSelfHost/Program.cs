2b0456b5d685b0f11b623b8ad1780ae2d197e9c2
Eric Candell <eric@candell.org>
2019-11-06 09:41:15 -0800

    Added DnsSelfHost project

---
+++ src/DnsSelfHost/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DnsSelfHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
